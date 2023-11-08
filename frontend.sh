8052eabcd2c7cd4219bbf2994a5b25ac16c7198d
G Jagadish <jguntumadugu474@gmail.com>
2023-11-08 16:13:25 +0530

    Added empty files and Content to Frontend.sh script

--- frontend.sh
+++ frontend.sh
@@ -1,6 +1,6 @@
 #The frontend is the service in Expense to serve the web content over Nginx. This will have the webframe for the web application.
 
-#This is a static content and to serve static content we need a web server. This server
+# This is a static content and to serve static content we need a web server. This server
 
 #Developer has chosen Nginx as a web server and thus we will install Nginx Web Server.
 
